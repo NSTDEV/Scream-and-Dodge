8213daa5b8bd65dfbecce93589a98f2cb3e7d85b
Nahuel Troncoso <92763369+NSTDEV@users.noreply.github.com>
2024-09-30 16:50:44 -0300

    Actualizacion de tilemap y script Parallax

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -5,13 +5,15 @@ public class Parallax : MonoBehaviour
     public float scrollSpeed = -5f;
     Vector3 startPos;
 
-    void Start() {
+    void Start()
+    {
         startPos = transform.position;
+        startPos.y = 14f;
     }
 
-    void Update() {
+    void Update()
+    {
         float newPos = Mathf.Repeat(Time.time * scrollSpeed, startPos.y);
         transform.position = new Vector3(startPos.x, startPos.y - newPos, startPos.z);
     }
-
 }
