73d65c14b9e7eb36558b5b080ba2ac9d00c8158b
Nahuel Troncoso <92763369+NSTDEV@users.noreply.github.com>
2024-09-30 02:16:25 -0300

    Parallax completo

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float scrollSpeed = -5f;
+    Vector3 startPos;
+
+    void Start() {
+        startPos = transform.position;
+    }
+
+    void Update() {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, startPos.y);
+        transform.position = new Vector3(startPos.x, startPos.y - newPos, startPos.z);
+    }
+
+}
