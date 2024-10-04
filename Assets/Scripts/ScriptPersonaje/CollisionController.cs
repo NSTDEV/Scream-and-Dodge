e95783f3573c4b0f5967a634c5013d1f112c3aa6
Nahuel Troncoso <92763369+NSTDEV@users.noreply.github.com>
2024-10-04 11:30:29 -0300

    Ajustes

--- Assets/Scripts/ScriptPersonaje/CollisionController.cs
+++ Assets/Scripts/ScriptPersonaje/CollisionController.cs
@@ -6,90 +6,72 @@ using UnityEngine;
 public class CollisionController : MonoBehaviour
 {
     public GameObject bear; // El oso
-    public float moveSpeed = 1.16f; // Velocidad de movimiento del oso
+    public float bearMoveSpeed = 0.16f; // Velocidad de movimiento del oso
+    public float playerMoveSpeed = 4f; // Velocidad de movimiento del jugador
     public float distanceThreshold = 3f; // Distancia mínima para colisionar
     private int collisionCount = 0; // Contador de colisiones
 
-     private VoiceCommandController voiceCommandController;
-
-     private Rigidbody2D rb;
+    private VoiceCommandController voiceCommandController;
 
     private void Start()
     {
         // Obtener la referencia al script VoiceCommandController
         voiceCommandController = GetComponent<VoiceCommandController>();
-
-         rb = GetComponent<Rigidbody2D>();
-
-        
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         // Verificamos si colisiona con uno de los obstáculos
-        if (collision.gameObject.CompareTag("Obstacle") || 
-            collision.gameObject.CompareTag("Obstacle2") ||
-            collision.gameObject.CompareTag("Obstacle3") || 
-            collision.gameObject.CompareTag("Obstacle4") )
+        if (other.gameObject.CompareTag("Obstacle") || other.gameObject.CompareTag("Obstacle2"))
         {
             collisionCount++;
 
-            // Mover al oso hacia el esquiador
-            MoveBearTowardsSkier();
-
-            // Si el jugador colisiona 3 veces, el oso se acercará lo suficiente o si colisionar con el arbol gigante perdera directamente
-            if (collisionCount >= 3 || collision.gameObject.CompareTag("Obstacle3") )
+            // Si el jugador colisiona 3 veces, el oso se acercará lo suficiente o si colisiona con el árbol gigante perderá directamente
+            if (collisionCount >= 3)
             {
-                
                 // Asegúrate de que el oso termine lo suficientemente cerca
                 float distance = Vector2.Distance(bear.transform.position, transform.position);
                 if (distance > distanceThreshold)
                 {
-                    bear.transform.position = Vector2.MoveTowards(bear.transform.position, transform.position, moveSpeed * Time.deltaTime);
+                    // Mueve al oso hacia el jugador
+                    bear.transform.position = Vector2.MoveTowards(bear.transform.position, transform.position, bearMoveSpeed * Time.deltaTime);
+                    print(distance);
                 }
-                // Cambiar de escena
-                SceneManager.LoadScene("Lose"); // Reemplaza con el nombre de la escena a la que quieres ir
-                
+                else
+                {
+                    // Cambiar de escena cuando el oso está lo suficientemente cerca
+                    SceneManager.LoadScene("Lose"); // Reemplaza con el nombre de la escena a la que quieres ir
+                }
             }
 
-           // Destruir el obstáculo al colisionar con el esquiador
-         Destroy(collision.gameObject);
+            // Mover al oso hacia el esquiador
+            MoveBearTowardsSkier();
+
+            // Destruir el obstáculo al colisionar con el esquiador
+            Destroy(other.gameObject);
         }
-         
     }
 
-    /**El metodo On TriggerEnter sirve para que al estar en modo Trigger aun asi choque con el obstaculo3 que aunque saltemos no lo esquivaremos*/
-    private void OnTriggerEnter2D(Collider2D other)
-{
-    // Verificar si el collider que se ha activado es el Obstacle3
-    if (other.CompareTag("Obstacle3"))
+    private void Update()
     {
-        // Asegúrate de que el oso termine lo suficientemente cerca
-                float distance = Vector2.Distance(bear.transform.position, transform.position);
-                if (distance > distanceThreshold)
-                {
-                    bear.transform.position = Vector2.MoveTowards(bear.transform.position, transform.position, moveSpeed * Time.deltaTime);
-                }
-        // Cambiar a la escena de perder
-        SceneManager.LoadScene("Lose"); // Asegúrate de que "Lose" sea el nombre correcto de tu escena
+        // Verificamos la posición en y de los obstáculos para destruirlos si están debajo del oso
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+        foreach (GameObject obstacle in obstacles)
+        {
+            if (obstacle.transform.position.y >= bear.transform.position.y - 4)
+            {
+                Destroy(obstacle);
+            }
+        }
     }
-}
 
     private void MoveBearTowardsSkier()
     {
-        // Mueve el oso hacia el esquiador en el eje Y
+        // Mueve al oso hacia el jugador/esquiador en ambos ejes (x e y)
         Vector2 bearPosition = bear.transform.position;
         Vector2 skierPosition = transform.position;
-        
-        if (bearPosition.y < skierPosition.y)
-        {
-            bearPosition.y += moveSpeed * Time.deltaTime;
-        }
-        else if (bearPosition.y > skierPosition.y)
-        {
-            bearPosition.y -= moveSpeed * Time.deltaTime;
-        }
 
-        bear.transform.position = bearPosition;
+        // Mover al oso hacia el jugador/esquiador de manera gradual
+        bear.transform.position = Vector2.MoveTowards(bearPosition, skierPosition, bearMoveSpeed * Time.deltaTime);
     }
 }
